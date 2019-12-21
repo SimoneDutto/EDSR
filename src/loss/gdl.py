167396c1c76560d66860dfb0da9b4c33f1a8ee29
SimoneDutto <simonedutto@gmail.com>
2019-12-21 10:29:19 +0100

    Fixiti

--- src/loss/gdl.py
+++ src/loss/gdl.py
@@ -6,7 +6,7 @@ import torch.optim as optim
 import torch
 import torch.nn.init
 
-import tensorflow as tf
+from keras import backend as K
 
 
 
@@ -17,25 +17,17 @@ class GDL(nn.Module):
 
     def forward(self, sr, hr):
         # create filters [-1, 1] and [[1],[-1]] for diffing to the left and down respectively.
-        pos = tf.constant(np.identity(1), dtype=tf.float32)
-        neg = -1 * pos
-        filter_x = tf.expand_dims(tf.stack([neg, pos]), 0)  # [-1, 1]
-        filter_y = tf.stack([tf.expand_dims(pos, 0), tf.expand_dims(neg, 0)])  # [[1],[-1]]
-        strides = [1, 1, 1, 1]  # stride of (1, 1)
-        padding = 'SAME'
-
-        gen_dx = tf.abs(tf.nn.conv2d(hr, filter_x, strides, padding=padding))
-        gen_dy = tf.abs(tf.nn.conv2d(hr, filter_y, strides, padding=padding))
-        gt_dx = tf.abs(tf.nn.conv2d(sr, filter_x, strides, padding=padding))
-        gt_dy = tf.abs(tf.nn.conv2d(sr, filter_y, strides, padding=padding))
-
-        grad_diff_x = tf.abs(gt_dx - gen_dx)
-        grad_diff_y = tf.abs(gt_dy - gen_dy)
-
-        gdl=tf.reduce_sum((grad_diff_x ** alpha + grad_diff_y ** alpha))
-
-        # condense into one tensor and avg
-        return gdl
+        y_true = K.batch_flatten(hr)
+        y_pred = K.batch_flatten(hr)
+        Y_true = K.reshape(y_true, (-1, ) + img_shape)
+        Y_pred = K.reshape(y_pred, (-1, ) + img_shape)
+        t1 = K.pow(K.abs(Y_true[:, :, 1:, :] - Y_true[:, :, :-1, :]) -
+                   K.abs(Y_pred[:, :, 1:, :] - Y_pred[:, :, :-1, :]), alpha)
+        t2 = K.pow(K.abs(Y_true[:, :, :, :-1] - Y_true[:, :, :, 1:]) -
+                   K.abs(Y_pred[:, :, :, :-1] - Y_pred[:, :, :, 1:]), alpha)
+        out = K.mean(K.batch_flatten(t1 + t2), -1)
+        
+        return out
 
 
 
