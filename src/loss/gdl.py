f85b36bf34e4ba7b5625c1d5680f34baf0cf46a7
SimoneDutto <simonedutto@gmail.com>
2019-12-21 10:36:51 +0100

    Fixiti

--- src/loss/gdl.py
+++ src/loss/gdl.py
@@ -6,7 +6,7 @@ import torch.optim as optim
 import torch
 import torch.nn.init
 
-from keras import backend as K
+import tensorflow as tf
 
 
 
@@ -16,18 +16,25 @@ class GDL(nn.Module):
         self.alpha = 1
 
     def forward(self, sr, hr):
-        # create filters [-1, 1] and [[1],[-1]] for diffing to the left and down respectively.
-        y_true = K.batch_flatten(hr.cpu().numpy())
-        y_pred = K.batch_flatten(hr.cpu().numpy())
-        Y_true = K.reshape(y_true, (-1, ) + img_shape)
-        Y_pred = K.reshape(y_pred, (-1, ) + img_shape)
-        t1 = K.pow(K.abs(Y_true[:, :, 1:, :] - Y_true[:, :, :-1, :]) -
-                   K.abs(Y_pred[:, :, 1:, :] - Y_pred[:, :, :-1, :]), alpha)
-        t2 = K.pow(K.abs(Y_true[:, :, :, :-1] - Y_true[:, :, :, 1:]) -
-                   K.abs(Y_pred[:, :, :, :-1] - Y_pred[:, :, :, 1:]), alpha)
-        out = K.mean(K.batch_flatten(t1 + t2), -1).cpu()
-        
-        return out
+        pos = tf.constant(np.identity(1), dtype=tf.float32)
+        neg = -1 * pos
+        filter_x = tf.expand_dims(tf.stack([neg, pos]), 0)  # [-1, 1]
+        filter_y = tf.stack([tf.expand_dims(pos, 0), tf.expand_dims(neg, 0)])  # [[1],[-1]]
+        strides = [1, 1, 1, 1]  # stride of (1, 1)
+        padding = 'SAME'
+
+        gen_dx = tf.abs(tf.nn.conv2d(gen_CT.cpu().numpy(), filter_x, strides, padding=padding))
+        gen_dy = tf.abs(tf.nn.conv2d(gen_CT.cpu().numpy(), filter_y, strides, padding=padding))
+        gt_dx = tf.abs(tf.nn.conv2d(gt_CT.cpu().numpy(), filter_x, strides, padding=padding))
+        gt_dy = tf.abs(tf.nn.conv2d(gt_CT.cpu().numpy(), filter_y, strides, padding=padding))
+
+        grad_diff_x = tf.abs(gt_dx - gen_dx)
+        grad_diff_y = tf.abs(gt_dy - gen_dy)
+
+        gdl=tf.reduce_sum((grad_diff_x ** alpha + grad_diff_y ** alpha))
+
+        return gdl
+
 
 
 
