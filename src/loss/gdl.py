401897e65c472814651eef95f44ae78d7059ed3d
SimoneDutto <simonedutto@gmail.com>
2019-12-21 11:09:21 +0100

    Add scaler

--- src/loss/gdl.py
+++ src/loss/gdl.py
@@ -16,22 +16,11 @@ class GDL(nn.Module):
         self.alpha = 1
 
     def forward(self, sr, hr):
-        pos = tf.constant(np.identity(1), dtype=tf.float32)
-        neg = -1 * pos
-        filter_x = tf.expand_dims(tf.stack([neg, pos]), 0)  # [-1, 1]
-        filter_y = tf.stack([tf.expand_dims(pos, 0), tf.expand_dims(neg, 0)])  # [[1],[-1]]
-        strides = [1, 1, 1, 1]  # stride of (1, 1)
-        padding = 'SAME'
-
-        gen_dx = tf.abs(tf.nn.conv2d(sr.cpu().detach().numpy(), filter_x, strides, padding=padding))
-        gen_dy = tf.abs(tf.nn.conv2d(sr.cpu().detach().numpy(), filter_y, strides, padding=padding))
-        gt_dx = tf.abs(tf.nn.conv2d(hr.cpu().detach().numpy(), filter_x, strides, padding=padding))
-        gt_dy = tf.abs(tf.nn.conv2d(hr.cpu().detach().numpy(), filter_y, strides, padding=padding))
-
-        grad_diff_x = tf.abs(gt_dx - gen_dx)
-        grad_diff_y = tf.abs(gt_dy - gen_dy)
-
-        gdl=tf.reduce_sum((grad_diff_x ** alpha + grad_diff_y ** alpha))
+        
+        t1 = K.pow(torch.abs(Y_true[:, :, :, 1:, :] - Y_true[:, :, :, :-1, :]) -
+                   K.abs(Y_pred[:, :, :, 1:, :] - Y_pred[:, :, :, :-1, :]), alpha)
+        t2 = K.pow(torch.abs(Y_true[:, :, :, :, :-1] - Y_true[:, :, :, :, 1:]) -
+                   K.abs(Y_pred[:, :, :, :, :-1] - Y_pred[:, :, :, :, 1:]), alpha)
 
         return gdl
 
