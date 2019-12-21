cc31dbc717257cd45a70e3e7ed1934c3c504fdb2
SimoneDutto <simonedutto@gmail.com>
2019-12-21 10:32:50 +0100

    Fixiti

--- src/loss/gdl.py
+++ src/loss/gdl.py
@@ -17,15 +17,15 @@ class GDL(nn.Module):
 
     def forward(self, sr, hr):
         # create filters [-1, 1] and [[1],[-1]] for diffing to the left and down respectively.
-        y_true = K.batch_flatten(hr)
-        y_pred = K.batch_flatten(hr)
-        Y_true = K.reshape(y_true, (-1, ) + img_shape).cpu().numpy()
-        Y_pred = K.reshape(y_pred, (-1, ) + img_shape).cpu().numpy()
+        y_true = K.batch_flatten(hr..cpu().numpy())
+        y_pred = K.batch_flatten(hr..cpu().numpy())
+        Y_true = K.reshape(y_true, (-1, ) + img_shape)
+        Y_pred = K.reshape(y_pred, (-1, ) + img_shape)
         t1 = K.pow(K.abs(Y_true[:, :, 1:, :] - Y_true[:, :, :-1, :]) -
-                   K.abs(Y_pred[:, :, 1:, :] - Y_pred[:, :, :-1, :]), alpha).cpu().numpy()
+                   K.abs(Y_pred[:, :, 1:, :] - Y_pred[:, :, :-1, :]), alpha)
         t2 = K.pow(K.abs(Y_true[:, :, :, :-1] - Y_true[:, :, :, 1:]) -
-                   K.abs(Y_pred[:, :, :, :-1] - Y_pred[:, :, :, 1:]), alpha).cpu().numpy()
-        out = K.mean(K.batch_flatten(t1 + t2), -1).cpu().numpy()
+                   K.abs(Y_pred[:, :, :, :-1] - Y_pred[:, :, :, 1:]), alpha)
+        out = K.mean(K.batch_flatten(t1 + t2), -1).cpu()
         
         return out
 
