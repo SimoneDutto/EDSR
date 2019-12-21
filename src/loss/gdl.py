1d751bf00c67a971a486cf8b54a8edc609e6814c
SimoneDutto <simonedutto@gmail.com>
2019-12-21 10:17:39 +0100

    Fixiti

--- src/loss/gdl.py
+++ src/loss/gdl.py
@@ -19,7 +19,7 @@ class GDL(nn.Module):
         # create filters [-1, 1] and [[1],[-1]] for diffing to the left and down respectively.
         pos = tf.constant(np.identity(1), dtype=tf.float32)
         neg = -1 * pos
-        filter_x = tf.expand_dims(tf.pack([neg, pos]), 0)  # [-1, 1]
+        filter_x = tf.expand_dims(tf.stack([neg, pos]), 0)  # [-1, 1]
         filter_y = tf.stack([tf.expand_dims(pos, 0), tf.expand_dims(neg, 0)])  # [[1],[-1]]
         strides = [1, 1, 1, 1]  # stride of (1, 1)
         padding = 'SAME'
