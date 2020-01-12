e9cc21505f6aa0148eddc84ec284908bbdb1439f
SimoneDutto <simonedutto@gmail.com>
2020-01-12 10:19:14 +0100

    Support for SVNH dataset

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -33,14 +33,16 @@ class Data:
 
         self.loader_test = []
         for d in args.data_test:
-            if d in ['SVHN','Set5', 'Set14', 'B100', 'Urban100']:
+            if d in ['Set5', 'Set14', 'B100', 'Urban100']:
                 m = import_module('data.benchmark')
                 testset = getattr(m, 'Benchmark')(args, train=False, name=d)
-            else:
+            else if d == "DIV2K":
                 module_name = d if d.find('DIV2K-Q') < 0 else 'DIV2KJPEG'
                 m = import_module('data.' + module_name.lower())
                 testset = getattr(m, module_name)(args, train=False, name=d)
-
+            else if d == "SVHN":
+                m = import_module('data.' + d.lower())
+                testset = getattr(m, module_name)(args, train=False, name=d)
             self.loader_test.append(
                 dataloader.DataLoader(
                     testset,
